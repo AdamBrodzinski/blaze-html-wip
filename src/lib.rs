d30d24fa079ed86d001012d56b4671d47ace6395
Adam Brodzinski <adambrodzinski@gmail.com>
2025-01-20 18:40:03 -0600

    feat: noop when data key is not found

--- src/lib.rs
+++ src/lib.rs
@@ -6,9 +6,12 @@ pub fn render_template_str(tmpl: &str, data: TmplData) -> String {
     let reg = Regex::new(r"@(\w+)").expect("Invalid regex");
 
     reg.replace_all(tmpl, |caps: &regex::Captures| {
-        let placeholder = caps.get(1).unwrap().as_str();
-        dbg!(placeholder);
-        data.get(placeholder).unwrap()
+        let key_name = caps.get(1).expect("Regex should have 1 capture").as_str();
+        match data.get(key_name) {
+            Some(x) => x.to_owned(),
+            // fallback to no change, @foo, if hashmap.get("foo") is empty
+            None => String::from(caps.get(0).unwrap().as_str()),
+        }
     })
     .to_string()
     //    .into_owned()
@@ -39,5 +42,16 @@ mod tests {
         let result = render_template_str(tmpl, data);
 
         assert_eq!(result, "name: Jane, age: 45");
+    }
+
+    #[test]
+    fn it_noops_when_data_is_not_found() {
+        let tmpl = "name: @name";
+        let mut data = TmplData::new();
+        data.insert("never".to_string(), "matches".to_string());
+
+        let result = render_template_str(tmpl, data);
+
+        assert_eq!(result, "name: @name");
     }
 }
