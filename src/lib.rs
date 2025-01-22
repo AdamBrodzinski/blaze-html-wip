40fed760d843b787c104bc716470a77d2157bdea
Adam Brodzinski <adambrodzinski@gmail.com>
2025-01-22 09:44:50 -0600

    wip: drop regex
    wip

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,68 @@
-use std::time::Instant;
-
-use regex::Regex;
 use serde_json::Value;
 
-pub fn render_template_str(tmpl: &str, data: serde_json::Value) -> String {
-    let start = Instant::now();
-    let reg = Regex::new(r"@(\w+)").expect("Invalid regex");
+pub fn render_template_str(template: &str, data: &serde_json::Value) -> String {
+    let mut result = String::with_capacity(template.len());
+    let mut chars = template.chars().peekable();
 
-    // for each template @var_name, look to see if the hashmap has the key "var_name"
-    // and replace with the hashmap value
-    let output = reg
-        .replace_all(tmpl, |caps: &regex::Captures| {
-            let key_name = caps.get(1).expect("expected 1 capture").as_str();
-            let entire_match = caps.get(0).unwrap().as_str();
-
-            match data.get(key_name) {
-                Some(Value::String(x)) => x.to_owned(),
-                Some(Value::Bool(x)) => x.to_string(),
-                Some(Value::Number(x)) => x.to_string(),
-                Some(Value::Null) => String::from(""),
-                Some(Value::Object(_)) => entire_match.to_string(),
-                // fallback to no change
-                Some(Value::Array(_)) => entire_match.to_string(),
-                None => entire_match.to_string(),
+    // iterate over the template once, and buildup a new output (String)
+    while let Some(c) = chars.next() {
+        if c == '@' {
+            // parse the @foo to get the json key name "foo"
+            let mut key = String::with_capacity(10);
+            while let Some(&next_c) = chars.peek() {
+                if next_c.is_ascii_alphanumeric() || next_c == '_' {
+                    key.push(next_c);
+                    chars.next();
+                } else {
+                    break;
+                }
             }
-        })
-        .to_string();
 
-    let duration = start.elapsed();
-    dbg!("Time taken: {:?}", duration);
-    output
+            if let Some(json_value) = data.get(&key) {
+                let value = match json_value {
+                    Value::String(x) => x.to_owned(),
+                    Value::Bool(x) => x.to_string(),
+                    Value::Number(x) => x.to_string(),
+                    Value::Null => String::new(),
+                    // fallback to no change
+                    Value::Object(_) => format!("@{}", key),
+                    Value::Array(_) => format!("@{}", key),
+                };
+                result.push_str(&value)
+            }
+            // if data was not found with key, retain the original "@foo"
+            else {
+                result.push('@');
+                result.push_str(&key);
+            }
+        }
+        // normal character, add it to the result string
+        else {
+            result.push(c);
+        }
+    }
+
+    result
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use serde_json::json;
+    use std::time::Instant;
 
-    #[test]
-    fn it_replaces_string_variable() {
-        let tmpl = "name: @name";
-        let data = json!({"name": "Jane"});
-
-        let result = render_template_str(tmpl, data);
-
-        assert_eq!(result, "name: Jane");
-    }
+    static NAME_AGE: &str = "name: @name, age: @age";
 
     #[test]
     fn it_replaces_two_variables() {
-        let tmpl = "name: @name, age: @age";
         let data = json!({"name": "Jane", "age": "45"});
 
-        let result = render_template_str(tmpl, data);
+        let start = Instant::now();
+
+        let result = render_template_str(NAME_AGE, &data);
+
+        let duration = start.elapsed();
+        println!("Time: {:?}", duration);
 
         assert_eq!(result, "name: Jane, age: 45");
     }
@@ -62,7 +72,7 @@ mod tests {
         let tmpl = "state: @is_open & is_on: false";
         let data = json!({"is_open": true, "is_on": false});
 
-        let result = render_template_str(tmpl, data);
+        let result = render_template_str(tmpl, &data);
 
         assert_eq!(result, "state: true & is_on: false");
     }
@@ -72,7 +82,7 @@ mod tests {
         let tmpl = "@a, @b, @c, @d";
         let data = json!({"a": 1, "b": 2.0, "c": -3, "d": 0.44});
 
-        let result = render_template_str(tmpl, data);
+        let result = render_template_str(tmpl, &data);
 
         assert_eq!(result, "1, 2.0, -3, 0.44");
     }
@@ -82,7 +92,7 @@ mod tests {
         let tmpl = "name: @name";
         let data = json!({"name": null});
 
-        let result = render_template_str(tmpl, data);
+        let result = render_template_str(tmpl, &data);
 
         assert_eq!(result, "name: ");
     }
@@ -92,7 +102,7 @@ mod tests {
         let tmpl = "name: @name";
         let data = json!({"never": "matches"});
 
-        let result = render_template_str(tmpl, data);
+        let result = render_template_str(tmpl, &data);
 
         assert_eq!(result, "name: @name");
     }
