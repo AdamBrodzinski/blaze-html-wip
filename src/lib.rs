aec54e89bafd11301fdf9ecdc7c766b65a1cd264
Adam Brodzinski <adambrodzinski@gmail.com>
2025-01-22 21:47:12 -0600

    feat: handles nested fields

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,25 @@
 use serde_json::Value;
 
+fn get_json_value<'a>(data: &'a Value, key: &str) -> Option<&'a Value> {
+    // current starts out as the json object
+    let mut current = data;
+
+    for segment in key.split('.') {
+        match current {
+            // check if current is an object
+            Value::Object(map) => {
+                // current for the last segment should be a non object value
+                current = map.get(segment)?;
+            }
+            // if current is not an object return early
+            // and don't change anything (noop)
+            _ => return None,
+        }
+    }
+
+    Some(current)
+}
+
 pub fn render_template_str(template: &str, data: &serde_json::Value) -> String {
     let mut result = String::with_capacity(template.len());
     let mut chars = template.chars().peekable();
@@ -10,7 +30,7 @@ pub fn render_template_str(template: &str, data: &serde_json::Value) -> String {
             // parse the @foo to get the json key name "foo"
             let mut key = String::with_capacity(10);
             while let Some(&next_c) = chars.peek() {
-                if next_c.is_ascii_alphanumeric() || next_c == '_' {
+                if next_c.is_ascii_alphanumeric() || next_c == '_' || next_c == '.' {
                     key.push(next_c);
                     chars.next();
                 } else {
@@ -18,7 +38,7 @@ pub fn render_template_str(template: &str, data: &serde_json::Value) -> String {
                 }
             }
 
-            if let Some(json_value) = data.get(&key) {
+            if let Some(json_value) = get_json_value(data, &key) {
                 let value = match json_value {
                     Value::String(x) => x.to_owned(),
                     Value::Bool(x) => x.to_string(),
@@ -51,19 +71,17 @@ mod tests {
     use serde_json::json;
     use std::time::Instant;
 
-    static NAME_AGE: &str = "name: @name, age: @age";
-
     #[test]
     fn it_replaces_two_variables() {
         let data = json!({"name": "Jane", "age": "45"});
+        let tmpl = "name: @name, age: @age";
 
         let start = Instant::now();
 
-        let result = render_template_str(NAME_AGE, &data);
+        let result = render_template_str(tmpl, &data);
 
         let duration = start.elapsed();
-        println!("Time: {:?}", duration);
-
+        println!("Simple Time: {:?}", duration);
         assert_eq!(result, "name: Jane, age: 45");
     }
 
@@ -72,8 +90,11 @@ mod tests {
         let tmpl = "state: @is_open & is_on: false";
         let data = json!({"is_open": true, "is_on": false});
 
+        let start = Instant::now();
         let result = render_template_str(tmpl, &data);
 
+        let duration = start.elapsed();
+        println!("Bool Time: {:?}", duration);
         assert_eq!(result, "state: true & is_on: false");
     }
 
@@ -107,13 +128,31 @@ mod tests {
         assert_eq!(result, "name: @name");
     }
 
-    // #[test]
-    // fn it_renders_each_blocks() {
-    //     let tmpl = r#"hello "world" bar "#; // foo
-    //                                         // let data = json!({"never": "matches"});
-    //                                         //
-    //                                         // let result = render_template_str(tmpl, data);
-    //
-    //     assert_eq!(tmpl, "name: @name");
-    // }
+    // ----------- nested fields -----------
+
+    #[test]
+    fn it_returns_nested_fields() {
+        let tmpl = "name: @person.name";
+        let data = json!({ "person": { "name": "Jane" } });
+        let start = Instant::now();
+
+        let result = render_template_str(tmpl, &data);
+
+        let duration = start.elapsed();
+        println!("Nested Time: {:?}", duration);
+        assert_eq!(result, "name: Jane");
+    }
+
+    #[test]
+    fn it_returns_deeply_nested_fields() {
+        let tmpl = "name: @a.b.c";
+        let data = json!({ "a": { "b": {"c": "foo"} } });
+        let start = Instant::now();
+
+        let result = render_template_str(tmpl, &data);
+
+        let duration = start.elapsed();
+        println!("Deeply Nested Time: {:?}", duration);
+        assert_eq!(result, "name: foo");
+    }
 }
