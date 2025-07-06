e5fd4cfccce59cca7d13b30a90b1a290deb11da1
Adam Brodzinski <adambrodzinski@gmail.com>
2025-07-05 21:52:21 -0500

    extract variables
    wip

--- src/lib.rs
+++ src/lib.rs
@@ -18,8 +18,10 @@
 
 use lol_html::html_content::{Comment, ContentType, Element};
 use lol_html::{doc_comments, element, rewrite_str, RewriteStrSettings};
-use serde_json::Value;
 use std::fs;
+
+mod variables;
+use variables::{get_json_value, replace_variables};
 
 pub fn render_template_str(template: &str, data: &serde_json::Value) -> String {
     let template = rewrite_html(template, data);
@@ -53,17 +55,15 @@ fn handle_component_elements(
     // extract the JSON data from the template_data by the props key, and set that
     // value as the "data" for the component. the component is effectively rendered
     // in isolation and then the final result replaces the component tag. This can
-    // be recursively called for the nested component use case.
-    let data = match el.get_attribute("props") {
+    // be recursively called for the nested component use case, but the nested template
+    // only has access to the data passed in from the parent
+    let conditional_data = match el.get_attribute("props") {
         Some(props_attr) => get_json_value(data, &props_attr)
             .unwrap_or_else(|| panic!("Could not find component props data with key {props_attr}")),
         None => data,
     };
-    let processed_contents = replace_variables(contents.trim_end(), data);
+    let processed_contents = replace_variables(contents.trim_end(), conditional_data);
 
-    // handle nested components
-    // todo, should data in this template be props or 'root' data
-    let processed_contents = render_template_str(&processed_contents, data);
     el.replace(&processed_contents, ContentType::Html);
     Ok(())
 }
@@ -73,161 +73,6 @@ fn remove_html_comments(
 ) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
     comment.remove();
     Ok(())
-}
-
-/// replaces any variables with the syntax "name: @name" with the value from Json({"name": "Jane"})
-/// with the result "name: Jane". Nested object values can be used with "@person.name.first"
-fn replace_variables(template: &str, data: &serde_json::Value) -> String {
-    let mut result = String::with_capacity(template.len());
-    let mut chars = template.chars().peekable();
-    let mut last_char = ' ';
-
-    // iterate over the template once, and buildup a new output (String)
-    while let Some(c) = chars.next() {
-        if c == '@' && !last_char.is_ascii_alphanumeric() {
-            // parse the @foo to get the json key name "foo"
-            let mut key = String::with_capacity(10);
-            while let Some(&next_c) = chars.peek() {
-                if next_c.is_ascii_alphanumeric() || next_c == '_' || next_c == '.' {
-                    key.push(next_c);
-                    chars.next();
-                } else {
-                    break;
-                }
-            }
-
-            // String type to satisfy match arms
-            let orig_var = || format!("@{}", key);
-
-            if let Some(json_value) = get_json_value(data, &key) {
-                let value = match json_value {
-                    Value::String(x) => x.to_owned(),
-                    Value::Bool(x) => x.to_string(),
-                    Value::Number(x) => x.to_string(),
-                    Value::Null => String::new(),
-                    // trying to access the entire object and using that as a template
-                    // value will not work, render noop instead
-                    Value::Object(_) => orig_var(),
-                    // todo
-                    Value::Array(_) => orig_var(),
-                };
-                result.push_str(&value)
-            }
-            // if data was not found with key, retain the original "@foo"
-            else {
-                result.push_str(orig_var().as_str());
-            }
-        }
-        // normal character, add it to the result string
-        else {
-            result.push(c);
-        }
-        last_char = c;
-    }
-
-    result
-}
-
-/// extracts the value out of a json data object. it is assumed that the
-/// json data is of type Object or it will return None, resulting in a noop
-fn get_json_value<'a>(data: &'a Value, key: &str) -> Option<&'a Value> {
-    // current starts out as the json object
-    let mut current = data;
-
-    for segment in key.split('.') {
-        match current {
-            // check if current is an object
-            Value::Object(map) => {
-                // current for the last segment should be a non object value
-                current = map.get(segment)?;
-            }
-            // if current is not an object return early
-            // and don't change anything (noop)
-            _ => return None,
-        }
-    }
-
-    Some(current)
-}
-
-#[cfg(test)]
-mod replace_variables_tests {
-    use super::*;
-    use serde_json::json;
-
-    #[test]
-    fn it_replaces_two_variables() {
-        let data = json!({"name": "Jane", "age": "45"});
-        let tmpl = "name: @name, age: @age";
-
-        let result = replace_variables(tmpl, &data);
-        assert_eq!(result, "name: Jane, age: 45");
-    }
-
-    #[test]
-    fn it_serializes_booleans() {
-        let tmpl = "state: @is_open & is_on: false";
-        let data = json!({"is_open": true, "is_on": false});
-
-        let result = replace_variables(tmpl, &data);
-        assert_eq!(result, "state: true & is_on: false");
-    }
-
-    #[test]
-    fn it_serializes_number() {
-        let tmpl = "@a, @b, @c, @d";
-        let data = json!({"a": 1, "b": 2.0, "c": -3, "d": 0.44});
-
-        let result = replace_variables(tmpl, &data);
-
-        assert_eq!(result, "1, 2.0, -3, 0.44");
-    }
-
-    #[test]
-    fn it_serializes_nul_as_empty_str() {
-        let tmpl = "name: @name";
-        let data = json!({"name": null});
-
-        let result = replace_variables(tmpl, &data);
-
-        assert_eq!(result, "name: ");
-    }
-
-    #[test]
-    fn it_noops_when_data_key_is_not_found() {
-        let tmpl = "name: @name";
-        let data = json!({"never": "matches"});
-
-        let result = replace_variables(tmpl, &data);
-
-        assert_eq!(result, "name: @name");
-    }
-
-    #[test]
-    fn does_not_transform_emails() {
-        let tmpl = "foo@bar baz";
-        let data = json!({ "bar": "Jane" });
-        let result = replace_variables(tmpl, &data);
-        assert_eq!(result, "foo@bar baz");
-    }
-
-    // ----------- nested fields -----------
-
-    #[test]
-    fn it_returns_nested_fields() {
-        let tmpl = "name: @person.name";
-        let data = json!({ "person": { "name": "Jane" } });
-        let result = replace_variables(tmpl, &data);
-        assert_eq!(result, "name: Jane");
-    }
-
-    #[test]
-    fn it_returns_deeply_nested_fields() {
-        let tmpl = "name: @a.b.c";
-        let data = json!({ "a": { "b": {"c": "foo"} } });
-        let result = replace_variables(tmpl, &data);
-        assert_eq!(result, "name: foo");
-    }
 }
 
 #[cfg(test)]
@@ -251,15 +96,6 @@ mod render_template_str_tests {
 
         let result = render_template_str(tmpl, &data);
         assert_eq!(result, "Hello <div>Component</div>");
-    }
-
-    #[test]
-    fn component_nested_test() {
-        let tmpl = "<component path='test_files/component-props-nested.html' />";
-        let data = json!(());
-
-        let result = render_template_str(tmpl, &data);
-        assert_eq!(result, "Nested <div>Component</div>");
     }
 
     #[test]
---
+++ src/variables.rs
@@ -0,0 +1,156 @@
+use serde_json::Value;
+
+/// replaces any variables with the syntax "name: @name" with the value from Json({"name": "Jane"})
+/// with the result "name: Jane". Nested object values can be used with "@person.name.first"
+pub fn replace_variables(template: &str, data: &serde_json::Value) -> String {
+    let mut result = String::with_capacity(template.len());
+    let mut chars = template.chars().peekable();
+    let mut last_char = ' ';
+
+    // iterate over the template once, and buildup a new output (String)
+    while let Some(c) = chars.next() {
+        if c == '@' && !last_char.is_ascii_alphanumeric() {
+            // parse the @foo to get the json key name "foo"
+            let mut key = String::with_capacity(10);
+            while let Some(&next_c) = chars.peek() {
+                if next_c.is_ascii_alphanumeric() || next_c == '_' || next_c == '.' {
+                    key.push(next_c);
+                    chars.next();
+                } else {
+                    break;
+                }
+            }
+
+            // String type to satisfy match arms
+            let orig_var = || format!("@{}", key);
+
+            if let Some(json_value) = get_json_value(data, &key) {
+                let value = match json_value {
+                    Value::String(x) => x.to_owned(),
+                    Value::Bool(x) => x.to_string(),
+                    Value::Number(x) => x.to_string(),
+                    Value::Null => String::new(),
+                    // trying to access the entire object and using that as a template
+                    // value will not work, render noop instead
+                    Value::Object(_) => orig_var(),
+                    // todo
+                    Value::Array(_) => orig_var(),
+                };
+                result.push_str(&value)
+            }
+            // if data was not found with key, retain the original "@foo"
+            else {
+                result.push_str(orig_var().as_str());
+            }
+        }
+        // normal character, add it to the result string
+        else {
+            result.push(c);
+        }
+        last_char = c;
+    }
+
+    result
+}
+
+/// extracts the value out of a json data object. it is assumed that the
+/// json data is of type Object or it will return None, resulting in a noop
+pub fn get_json_value<'a>(data: &'a Value, key: &str) -> Option<&'a Value> {
+    // current starts out as the json object
+    let mut current = data;
+
+    for segment in key.split('.') {
+        match current {
+            // check if current is an object
+            Value::Object(map) => {
+                // current for the last segment should be a non object value
+                current = map.get(segment)?;
+            }
+            // if current is not an object return early
+            // and don't change anything (noop)
+            _ => return None,
+        }
+    }
+
+    Some(current)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+
+    #[test]
+    fn it_replaces_two_variables() {
+        let data = json!({"name": "Jane", "age": "45"});
+        let tmpl = "name: @name, age: @age";
+
+        let result = replace_variables(tmpl, &data);
+        assert_eq!(result, "name: Jane, age: 45");
+    }
+
+    #[test]
+    fn it_serializes_booleans() {
+        let tmpl = "state: @is_open & is_on: false";
+        let data = json!({"is_open": true, "is_on": false});
+
+        let result = replace_variables(tmpl, &data);
+        assert_eq!(result, "state: true & is_on: false");
+    }
+
+    #[test]
+    fn it_serializes_number() {
+        let tmpl = "@a, @b, @c, @d";
+        let data = json!({"a": 1, "b": 2.0, "c": -3, "d": 0.44});
+
+        let result = replace_variables(tmpl, &data);
+
+        assert_eq!(result, "1, 2.0, -3, 0.44");
+    }
+
+    #[test]
+    fn it_serializes_nul_as_empty_str() {
+        let tmpl = "name: @name";
+        let data = json!({"name": null});
+
+        let result = replace_variables(tmpl, &data);
+
+        assert_eq!(result, "name: ");
+    }
+
+    #[test]
+    fn it_noops_when_data_key_is_not_found() {
+        let tmpl = "name: @name";
+        let data = json!({"never": "matches"});
+
+        let result = replace_variables(tmpl, &data);
+
+        assert_eq!(result, "name: @name");
+    }
+
+    #[test]
+    fn does_not_transform_emails() {
+        let tmpl = "foo@bar baz";
+        let data = json!({ "bar": "Jane" });
+        let result = replace_variables(tmpl, &data);
+        assert_eq!(result, "foo@bar baz");
+    }
+
+    // ----------- nested fields -----------
+
+    #[test]
+    fn it_returns_nested_fields() {
+        let tmpl = "name: @person.name";
+        let data = json!({ "person": { "name": "Jane" } });
+        let result = replace_variables(tmpl, &data);
+        assert_eq!(result, "name: Jane");
+    }
+
+    #[test]
+    fn it_returns_deeply_nested_fields() {
+        let tmpl = "name: @a.b.c";
+        let data = json!({ "a": { "b": {"c": "foo"} } });
+        let result = replace_variables(tmpl, &data);
+        assert_eq!(result, "name: foo");
+    }
+}
