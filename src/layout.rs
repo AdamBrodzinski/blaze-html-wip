8a7edba1cdbef49c509aec4e9f9ebe6c4a84e208
Adam Brodzinski <adambrodzinski@gmail.com>
2025-12-22 21:35:43 -0600

    parse layout
    wip

--- src/layout.rs
+++ src/layout.rs
@@ -1,9 +1,12 @@
 #![allow(unused)]
 use nom::bytes::complete::{take_till1, take_until};
+use nom::character::complete::{anychar, char, space0};
+use nom::combinator::{peek, recognize, rest, value};
+use nom::multi::many_till;
 use nom::sequence::delimited;
 use nom::Parser;
 use nom::{branch::alt, multi::many0};
-use nom::{bytes::complete::tag, sequence::preceded};
+use nom::{bytes::complete::tag, character, sequence::preceded};
 use nom::{bytes::complete::take_while1, IResult};
 use serde_json::Value;
 
@@ -28,7 +31,6 @@ pub fn transform_layout(
     let output = page_template_str.to_string();
     let layout_content = layout_template_str.to_owned();
     let (_input, layout_tag_inner) = extract_layout_content(page_template_str).unwrap();
-    dbg!(layout_tag_inner);
     let (_, layout_content) = extract_layout_start_end(layout_content.as_str()).unwrap();
 
     Ok(format!(
@@ -43,27 +45,28 @@ fn extract_layout_content(input: &str) -> IResult<&str, &str> {
 }
 
 fn extract_layout_start_end(input: &str) -> IResult<&str, LayoutContent> {
-    Ok((
-        input,
-        LayoutContent {
-            start: "START",
-            end: "END",
-        },
-    ))
+    // everything up to (not including) where slot_tag starts
+    let (input, start) = recognize(many_till(anychar, peek(slot_tag))).parse(input)?;
+    let (input, _) = slot_tag(input)?;
+    let (input, end) = rest(input)?;
+    Ok((input, LayoutContent { start, end }))
 }
 
-/*
-
-output
-"Before Middle After"
-
-let textbefore = takeuntiltag0(|tag|  tag == "<include-script")
-let slot = combo(tag("<slot"), emptyspace0, preceeded(space0, tag("/>")))
-let textafter = take0()
-
-let new_page_str = transform_layout(layout_str, page_str)
-
-*/
+fn slot_tag(input: &str) -> IResult<&str, ()> {
+    value(
+        (),
+        (
+            char('<'),
+            space0,
+            tag("slot"),
+            space0,
+            char('/'),
+            space0,
+            char('>'),
+        ),
+    )
+    .parse(input)
+}
 
 #[cfg(test)]
 mod tests {
@@ -71,7 +74,25 @@ mod tests {
     use serde_json::json;
 
     #[test]
-    fn it_replaces_variables() {
+    fn it_transforms_simple_layout() {
+        let data = json!(());
+        let page_tmpl = "<layout>Content</layout>";
+        let layout_tmpl = "Header <slot /> Footer";
+        let result = transform_layout(page_tmpl, layout_tmpl, &data);
+        assert_eq!(result.unwrap(), "Header Content Footer");
+    }
+
+    #[test]
+    fn it_transforms_layout_slot_tag_with_spaces() {
+        let data = json!(());
+        let page_tmpl = "<layout>Content</layout>";
+        let layout_tmpl = "Header <  slot   / > Footer";
+        let result = transform_layout(page_tmpl, layout_tmpl, &data);
+        assert_eq!(result.unwrap(), "Header Content Footer");
+    }
+
+    #[test]
+    fn it_transforms_layout_slot_tag_with_no_spaces() {
         let data = json!(());
         let page_tmpl = "<layout>Content</layout>";
         let layout_tmpl = "Header <slot/> Footer";
