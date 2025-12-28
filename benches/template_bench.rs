a1b264e65461fbbb0ffd5c53fba0f8981c20d37e
Adam Brodzinski <adambrodzinski@gmail.com>
2025-12-27 23:07:38 -0600

    fix bench

--- benches/template_bench.rs
+++ benches/template_bench.rs
@@ -70,10 +70,15 @@ fn bench_many_component(c: &mut Criterion) {
 
 fn bench_many_each(c: &mut Criterion) {
     let blaze = setup();
-    // Generate data with 60 arrays, each containing 5 items
+    // Generate data with 60 arrays, each containing 1 item
     let mut data = serde_json::Map::new();
-    let items: Vec<serde_json::Value> = (0..1).map(|_| json!({"name": "Jane"})).collect();
-    data.insert(String::from("items"), serde_json::Value::Array(items));
+    let items: Vec<serde_json::Value> = vec![json!({"name": "Jane"})];
+    for i in 0..60 {
+        data.insert(
+            format!("items{}", i),
+            serde_json::Value::Array(items.clone()),
+        );
+    }
     let data = serde_json::Value::Object(data);
 
     c.bench_function("many_each", |b| {
