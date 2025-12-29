66eb3d165ec5ccf15a50e7f1e2522b42ee916410
Adam Brodzinski <adambrodzinski@gmail.com>
2025-12-28 22:27:10 -0600

    rename tag

--- src/each_twice.rs
+++ src/each_twice.rs
@@ -14,6 +14,25 @@ pub enum Node<'a> {
     EachTwo(Vec<Node<'a>>),
 }
 
+fn process_each(input: &str) -> Result<String, String> {
+    let mut buffer = String::new();
+    let (_, nodes) = document(input.as_bytes()).map_err(|e| e.to_string())?;
+
+    for node in nodes {
+        dbg!(node);
+        match node {
+            Text(txt) => {
+                buffer.push_str("Text");
+            }
+            EachTwo(txt) => {
+                buffer.push_str("Each");
+            }
+        }
+    }
+
+    Ok(String::from(""))
+}
+
 fn node(input: &'_ Bytes) -> IResult<&'_ Bytes, Node<'_>> {
     alt((each_two, text_node)).parse(input)
 }
@@ -37,7 +56,7 @@ fn text_node(input: &Bytes) -> IResult<&Bytes, Node<'_>> {
     while i < len {
         if input[i] == b'<' {
             let rest = &input[i..];
-            if rest.starts_with(b"<each-two>") || rest.starts_with(b"</each-two>") {
+            if rest.starts_with(b"<Each>") || rest.starts_with(b"</Each>") {
                 break;
             }
         }
@@ -54,8 +73,8 @@ fn text_node(input: &Bytes) -> IResult<&Bytes, Node<'_>> {
 //     // note, this works and test passes, 6 micro seconds
 //     let (input, matched) = verify(
 //         alt((
-//             take_until(b"<each-two>" as &Bytes),
-//             take_until(b"</each-two>" as &Bytes),
+//             take_until(b"<Each>" as &Bytes),
+//             take_until(b"</Each>" as &Bytes),
 //             rest,
 //         )),
 //         |s: &Bytes| !s.is_empty(),
@@ -67,11 +86,11 @@ fn text_node(input: &Bytes) -> IResult<&Bytes, Node<'_>> {
 // }
 
 fn open_each(input: &Bytes) -> IResult<&Bytes, &Bytes> {
-    tag(b"<each-two>" as &Bytes).parse(input)
+    tag(b"<Each>" as &Bytes).parse(input)
 }
 
 fn close_each(input: &Bytes) -> IResult<&Bytes, &Bytes> {
-    tag(b"</each-two>" as &Bytes).parse(input)
+    tag(b"</Each>" as &Bytes).parse(input)
 }
 
 #[cfg(test)]
@@ -79,13 +98,23 @@ mod tests {
     use super::*;
     use serde_json::json;
 
+    mod process {
+        use super::*;
+
+        #[test]
+        fn test_process_each() {
+            let result = process_each("First <Each>Inner</Each> Last").unwrap();
+            assert_eq!(result, "foo");
+        }
+    }
+
     mod text {
         use super::*;
         #[test]
         fn text_and_each_tag() {
-            let (remaining, text) = text_node(b"foo <each-two>").unwrap();
+            let (remaining, text) = text_node(b"foo <Each>").unwrap();
             assert_eq!(text, Node::Text(b"foo "));
-            assert_eq!(remaining, b"<each-two>");
+            assert_eq!(remaining, b"<Each>");
         }
 
         #[test]
@@ -99,31 +128,36 @@ mod tests {
         use super::*;
 
         #[test]
+        fn test_process_each() {
+            let result = process_each("First <Each>Inner</Each> Last").unwrap();
+            assert_eq!(result, "foo");
+        }
+
+        #[test]
         fn test_document_input() {
-            let (remaining, nodes) = document(b"First <each-two>Inner</each-two> Last").unwrap();
+            let (remaining, nodes) = document(b"First <Each>Inner</Each> Last").unwrap();
             assert_eq!(remaining, b"");
         }
 
         #[test]
         fn test_nested_document_input() {
-            let (remaining, nodes) =
-                document(b"BeforeOuter<each-two>BeforeInner<each-two>Inner2</each-two>AfterInner</each-two> AfterOuter")
-                    .unwrap();
-            assert_eq!(remaining, b"");
-        }
-
-        #[test]
-        fn test_nested_html_input() {
             let (remaining, nodes) = document(
-                b"<b>First</b> <each-two>Inner1<each-two>Inner2</each-two></each-two> Last",
+                b"BeforeOuter<Each>BeforeInner<Each>Inner2</Each>AfterInner</Each> AfterOuter",
             )
             .unwrap();
             assert_eq!(remaining, b"");
         }
 
         #[test]
+        fn test_nested_html_input() {
+            let (remaining, nodes) =
+                document(b"<b>First</b> <Each>Inner1<Each>Inner2</Each></Each> Last").unwrap();
+            assert_eq!(remaining, b"");
+        }
+
+        #[test]
         fn deep_nesting_with_text() {
-            let input = b"<each-two>a<each-two>b<each-two>c</each-two>d</each-two>e</each-two>";
+            let input = b"<Each>a<Each>b<Each>c</Each>d</Each>e</Each>";
             let (remaining, nodes) = document(input).unwrap();
             assert_eq!(remaining, b"");
         }
