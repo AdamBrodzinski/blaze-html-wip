f07f1865c5b1e0b17d8c0fa52f0364c76aa5447b
Adam Brodzinski <adambrodzinski@gmail.com>
2026-01-04 17:05:25 -0600

    refactor bench tests
    fix
    
    fix each

--- benches/template_bench.rs
+++ benches/template_bench.rs
@@ -1,157 +1,67 @@
-use blaze_html::each::{document, process_each};
 use blaze_html::BlazeTemplate;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use serde_json::json;
 
-fn bench_single_each_tag(c: &mut Criterion) {
-    let input = br#"<Each items="@items">Inner content</Each>"#;
-
-    c.bench_function("single_each_tag", |b| b.iter(|| document(black_box(input))));
-}
-
-fn bench_sixty_each_tags(c: &mut Criterion) {
-    // Build a string with 60 each tags, each on a new line
-    let input: String = (0..60)
-        .map(|i| format!(r#"<Each items="@items{i}">Content {i}</Each>"#))
-        .collect::<Vec<_>>()
-        .join("\n");
-
-    c.bench_function("sixty_each_tags", |b| {
-        b.iter(|| document(black_box(input.as_bytes())))
-    });
-}
-
-fn bench_process_sixty_each_tags(c: &mut Criterion) {
-    // Build a page template with 60 each tags
-    let input: String = (0..60)
-        .map(|i| format!(r#"<Each items="@items{i}">@i Content {i}</Each>"#))
-        .collect::<Vec<_>>()
-        .join("\n");
-
-    // Build data with 60 arrays
-    let mut data = serde_json::Map::new();
-    for i in 0..60 {
-        data.insert(format!("items{i}"), json!([1, 2, 3]));
-    }
-    let data = serde_json::Value::Object(data);
-
-    c.bench_function("process_sixty_each_tags", |b| {
-        b.iter(|| process_each(black_box(&input), black_box(&data)))
-    });
-}
-
-fn setup() -> BlazeTemplate {
+fn setup_blaze_engine() -> BlazeTemplate {
     BlazeTemplate::builder()
         .set_root_directory("test_files")
         .register_component("Card", "components/card.html")
         .build()
 }
 
-fn bench_simple_variables(c: &mut Criterion) {
-    let blaze = setup();
-    let data = json!({"name": "Jane", "age": "45"});
+// renders 60 <Card>content</Card> components
+fn bench_simple_component(c: &mut Criterion) {
+    let blaze = setup_blaze_engine();
+    let data = json!(());
 
-    c.bench_function("simple_variables", |b| {
-        b.iter(|| blaze.render_page(black_box("bench/simple_vars.html"), black_box(&data)))
+    c.bench_function("simple_component", |b| {
+        b.iter(|| blaze.render_page(black_box("bench/component_simple.html"), black_box(&data)))
     });
 }
 
-fn bench_deeply_nested_fields(c: &mut Criterion) {
-    let blaze = setup();
-    let data = json!({"name": "Jane", "age": 30});
+// renders 60 <Each>@name</Each> components
+fn bench_simple_each(c: &mut Criterion) {
+    let blaze = setup_blaze_engine();
+    let data = json!({
+        "items": [
+            {"name": "Name 1"},
+            {"name": "Name 2"},
+            {"name": "Name 3"},
+            {"name": "Name 4"},
+        ],
+    });
 
-    c.bench_function("deeply_nested_fields", |b| {
-        b.iter(|| blaze.render_page(black_box("bench/nested_vars.html"), black_box(&data)))
+    c.bench_function("simple_each", |b| {
+        b.iter(|| blaze.render_page(black_box("bench/each_simple.html"), black_box(&data)))
     });
 }
 
-fn bench_escaped_at_symbols(c: &mut Criterion) {
-    let blaze = setup();
-    let data = json!({});
+// renders nested <Each> with inner <Each> loop
+fn bench_nested_each(c: &mut Criterion) {
+    let blaze = setup_blaze_engine();
+    let data = json!({
+        "items": [
+            {
+                "item_name": "Foo",
+                "item_things": [
+                    {"name": "Name 1"},
+                    {"name": "Name 2"},
+                    {"name": "Name 3"},
+                    {"name": "Name 4"},
+                ],
+            },
+        ],
+    });
 
-    c.bench_function("escaped_at_symbols", |b| {
-        b.iter(|| blaze.render_page(black_box("bench/escaped.html"), black_box(&data)))
+    c.bench_function("nested_each", |b| {
+        b.iter(|| blaze.render_page(black_box("bench/each_nested.html"), black_box(&data)))
     });
 }
-
-fn bench_many_component(c: &mut Criterion) {
-    let blaze = setup();
-    let data = json!({"name": "Jane", "age": 30});
-
-    c.bench_function("many_component", |b| {
-        b.iter(|| {
-            blaze.render_page(
-                black_box("bench/with_many_component.html"),
-                black_box(&data),
-            )
-        })
-    });
-}
-
-fn bench_each_two(c: &mut Criterion) {
-    let blaze = setup();
-    let data = json!({"items": [{"name": "Jane"}]});
-
-    c.bench_function("each_two", |b| {
-        b.iter(|| blaze.render_page(black_box("bench/each_two.html"), black_box(&data)))
-    });
-}
-
-fn bench_many_each(c: &mut Criterion) {
-    let blaze = setup();
-    // Generate data with 60 arrays, each containing 1 item
-    let mut data = serde_json::Map::new();
-    let items: Vec<serde_json::Value> = vec![json!({"name": "Jane"})];
-    for i in 0..60 {
-        data.insert(
-            format!("items{}", i),
-            serde_json::Value::Array(items.clone()),
-        );
-    }
-    let data = serde_json::Value::Object(data);
-
-    c.bench_function("many_each", |b| {
-        b.iter(|| blaze.render_page(black_box("bench/many_each.html"), black_box(&data)))
-    });
-}
-
-// fn bench_each_tag_simple(c: &mut Criterion) {
-//     let tmpl = "<each>Content to extract</each>";
-//     let data = json!(());
-//
-//     c.bench_function("each_tag_simple", |b| {
-//         b.iter(|| render_template_str(black_box(tmpl), black_box(&data)))
-//     });
-// }
-//
-// fn bench_each_tag_nested(c: &mut Criterion) {
-//     let tmpl = "<each>Outer <each>Inner</each> More</each>";
-//     let data = json!(());
-//
-//     c.bench_function("each_tag_nested", |b| {
-//         b.iter(|| render_template_str(black_box(tmpl), black_box(&data)))
-//     });
-// }
-//
-// fn bench_each_tag_multiple(c: &mut Criterion) {
-//     let tmpl = "<each>First</each> <each>Second</each> <each>Third</each>";
-//     let data = json!(());
-//
-//     c.bench_function("each_tag_multiple", |b| {
-//         b.iter(|| render_template_str(black_box(tmpl), black_box(&data)))
-//     });
-// }
 
 criterion_group!(
     benches,
-    bench_single_each_tag,
-    bench_sixty_each_tags,
-    bench_process_sixty_each_tags,
-    bench_each_two,
-    bench_many_each,
-    bench_many_component,
-    bench_simple_variables,
-    bench_deeply_nested_fields,
-    bench_escaped_at_symbols,
+    bench_simple_component,
+    bench_simple_each,
+    bench_nested_each,
 );
 criterion_main!(benches);
