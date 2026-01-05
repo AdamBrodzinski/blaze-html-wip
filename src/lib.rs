3f022ec09bd01d9fa7cf510a7cc765da073352ba
Adam Brodzinski <adambrodzinski@gmail.com>
2026-01-05 13:49:09 -0600

    remove old code

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,3 @@
-//! # Blaze HTML
-//!
-//! This crate provides another syntax for html templating that works with any
-//! HTML formatter/parser. The syntax is similar to web components and tries to
-//!
-//! ## Features
-//! - Variable replacement with context
-//!
-//!
-// ## Example
-// ```rust
-// use my_crate::do_foo;
-// do_foo();
-// ```
-
+mod asset;
 mod ast;
-mod build_template;
-mod component;
-mod data;
-pub mod each;
-mod parsers;
-mod template;
-mod variables;
-
-pub use template::BlazeTemplate;
+mod shared_parsers;
