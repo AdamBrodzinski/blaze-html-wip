fdaf3bb4d41c5c9304847cf5777dc39e64d1f2a8
Adam Brodzinski <adambrodzinski@gmail.com>
2026-01-06 18:55:29 -0600

    parse page template into ast
    wip

---
+++ src/parse_template.rs
@@ -0,0 +1,44 @@
+use nom::Parser;
+use nom::{branch::alt, multi::many0};
+use serde_json::Value;
+
+use crate::{asset, ast::TemplateNode};
+
+/// Parse and render a template in a single pass
+pub fn render_template(page_template: &str, data: &Value) -> Result<String, String> {
+    let ast_nodes = parse_template_to_ast(page_template, data).map_err(|e| e.to_string())?;
+    render_ast(ast_nodes)
+}
+
+fn parse_template_to_ast(page_template: &str, data: &Value) -> Result<Vec<TemplateNode>, String> {
+    let (input, _) = many0(asset::parse_script)
+        .parse(page_template)
+        .map_err(|e| e.to_string())?;
+
+    Ok(Vec::new())
+}
+
+fn render_ast(ast: Vec<TemplateNode>) -> Result<String, String> {
+    dbg!(ast);
+    Ok(String::from("TODO"))
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    mod script {
+        use super::*;
+        use serde_json::json;
+
+        #[test]
+        fn minimal() {
+            let template = r#"<Script path="static/bar.js" /> After"#;
+            let data = json!(());
+            let ast = parse_template_to_ast(template, &data).unwrap();
+            // TODO test to make sure AST matches
+            assert_eq!(ast.len(), 2);
+            assert_eq!(ast, []);
+        }
+    }
+}
