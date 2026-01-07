756049c228059ec1a0bfcd4cd69af2b3071aa174
Adam Brodzinski <adambrodzinski@gmail.com>
2026-01-07 09:49:15 -0600

    refactor asset tag

--- src/tag_asset.rs
+++ src/tag_asset.rs
@@ -19,34 +19,36 @@ pub fn parse_script(input: &str) -> IResult<&str, TemplateNode> {
             pair(multispace0, tag("/>")),
         ),
         |(_, attrs, _)| {
-            let mut src_path: Option<&str> = None;
-
-            let mut passthrough_attrs = String::with_capacity(10 * attrs.len());
-            for (key, value) in attrs {
-                if key == "path" {
-                    src_path = Some(value);
-                    continue;
-                }
-                passthrough_attrs.push(' ');
-                passthrough_attrs.push_str(key);
-                passthrough_attrs.push('=');
-                passthrough_attrs.push('"');
-                passthrough_attrs.push_str(value);
-                passthrough_attrs.push('"');
-            }
-
-            let src_path = match src_path {
-                Some(path) => path,
-                None => panic!("'path' is a required field of Script tag"),
-            };
-            let text = format!(
-                r#"<script src="{}"{}></script>"#,
-                src_path, passthrough_attrs
-            );
+            let (src_path, other_attrs) = separate_path_attr(attrs);
+            let text = format!(r#"<script src="{}"{}></script>"#, src_path, other_attrs);
             TemplateNode::Asset(text)
         },
     )
     .parse(input)
+}
+
+fn separate_path_attr<'a>(attrs: Vec<(&str, &'a str)>) -> (&'a str, String) {
+    let mut src_path: Option<&str> = None;
+
+    let mut passthrough_attrs = String::with_capacity(10 * attrs.len());
+    for (key, value) in attrs {
+        if key == "path" {
+            src_path = Some(value);
+            continue;
+        }
+        passthrough_attrs.push(' ');
+        passthrough_attrs.push_str(key);
+        passthrough_attrs.push('=');
+        passthrough_attrs.push('"');
+        passthrough_attrs.push_str(value);
+        passthrough_attrs.push('"');
+    }
+
+    let src_path = match src_path {
+        Some(path) => path,
+        None => panic!("'path' is a required field of Script tag"),
+    };
+    (src_path, passthrough_attrs)
 }
 
 #[cfg(test)]
