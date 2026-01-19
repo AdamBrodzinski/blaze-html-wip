5a5f8316c2661b74884036c70bb401ff016f7ee1
Adam Brodzinski <adambrodzinski@gmail.com>
2026-01-19 16:26:40 -0600

    Bench

--- benches/template_bench.rs
+++ benches/template_bench.rs
@@ -103,13 +103,55 @@ fn bench_each(c: &mut Criterion) {
     });
 }
 
+fn bench_component(c: &mut Criterion) {
+    let blaze = BlazeTemplate::builder()
+        .template_root_dir("test_files")
+        .build();
+
+    blaze
+        .register_component("Person", "components/Person.html")
+        .unwrap();
+
+    blaze
+        .compile_page_template("pages/bench_component.html")
+        .unwrap();
+
+    let data = json!({ "data": { "age": 30 } });
+
+    c.bench_function("component", |b| {
+        b.iter(|| blaze.render_page(black_box("pages/bench_component.html"), &data))
+    });
+}
+
+fn bench_component_simple(c: &mut Criterion) {
+    let blaze = BlazeTemplate::builder()
+        .template_root_dir("test_files")
+        .build();
+
+    blaze
+        .register_component("Button", "components/button.html")
+        .unwrap();
+
+    blaze
+        .compile_page_template("pages/bench_component_simple.html")
+        .unwrap();
+
+    let data = json!({});
+
+    c.bench_function("component simple", |b| {
+        b.iter(|| blaze.render_page(black_box("pages/bench_component_simple.html"), &data))
+    });
+}
+
 criterion_group!(
     benches,
+    bench_component_simple,
+    bench_component,
     bench_plain_text,
     bench_asset_precompiled,
     bench_variables,
     bench_if_true,
     bench_if_truthy,
-    bench_each
+    bench_each,
 );
 criterion_main!(benches);
